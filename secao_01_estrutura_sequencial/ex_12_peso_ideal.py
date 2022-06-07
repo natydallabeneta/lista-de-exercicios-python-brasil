9a997b2857192e6db28a7878d2f6f43f69b7408a
NataliaDallabeneta <natalia@dallabeneta.com.br>
2022-06-07 12:08:20 +0200

    Resolução dosExercício 11

--- secao_01_estrutura_sequencial/ex_12_peso_ideal.py
+++ secao_01_estrutura_sequencial/ex_12_peso_ideal.py
@@ -19,3 +19,7 @@ Mostrar a área com 1 casa decimal.
 
 def calcular_peso_ideal():
     """Escreva aqui em baixo a sua solução"""
+
+    altura = (float(input('Digite a sua altura: ')))
+    print(f'Seu peso ideal é {(72.7*altura) - 58:.1f} kg')
+

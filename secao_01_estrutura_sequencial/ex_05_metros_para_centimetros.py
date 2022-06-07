11ea10326b6551db89c39fff7a40060a6d9ffa97
NataliaDallabeneta <natalia@dallabeneta.com.br>
2022-06-07 09:46:32 +0200

    Resolução do Exercício 5

--- secao_01_estrutura_sequencial/ex_05_metros_para_centimetros.py
+++ secao_01_estrutura_sequencial/ex_05_metros_para_centimetros.py
@@ -17,6 +17,6 @@ Faça um Programa que converta metros para centímetros.
 def converter_metros_para_centimetros():
     """Escreva aqui em baixo a sua solução"""
 
-    metros = 1
+    metros = float(input('Digite o valor em metros: '))
     metros_para_centimetros = float(metros*100)
     print(f'Transformando para centímetros dá {metros_para_centimetros} cm')

1a82b614c19116ed84f0fea1666999b402394962
NataliaDallabeneta <natalia@dallabeneta.com.br>
2022-06-07 13:44:25 +0200

    Resolução dosExercício 16

--- secao_01_estrutura_sequencial/ex_16_loja_de_tintas_simples.py
+++ secao_01_estrutura_sequencial/ex_16_loja_de_tintas_simples.py
@@ -21,3 +21,13 @@ Considere que a cobertura da tinta é de 1 litro para cada 3 metros quadrados e 
 
 def calcular_latas_e_preco_de_tinta():
     """Escreva aqui em baixo a sua solução"""
+
+
+    tamanho_em_metros_quadrados = int(input('Digite o tamanho em metros quadrados da área a ser pintada: '))
+    cobertura_lata = 3 * 18
+    valor_lata = 80.00
+    litros_necessarios = tamanho_em_metros_quadrados/cobertura_lata
+    latas_necessarias = round(litros_necessarios % cobertura_lata)
+    custo_total = latas_necessarias * valor_lata
+
+    print(f'Você deve comprar {latas_necessarias} lata(s) tinta ao custo de R$ {custo_total:.2f}')

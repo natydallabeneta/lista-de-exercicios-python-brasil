b136adc7f95ac9a72ebc15023e0b00efcb947c43
NataliaDallabeneta <natalia@dallabeneta.com.br>
2022-06-08 16:00:13 +0200

    Exercicio 02

--- secao_02_estrutura_de_decisao/ex_02_positivo_ou_negativo.py
+++ secao_02_estrutura_de_decisao/ex_02_positivo_ou_negativo.py
@@ -17,3 +17,12 @@ Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou 
 
 def positivo_ou_negativo(n):
     """Escreva aqui em baixo a sua solução"""
+
+    n = int(input('Digite um valor: '))
+
+    if n == 0:
+        return 'não nem positivo nem negativo'
+    elif n < 0:
+        return 'negativo'
+    else:
+        return 'positivo'

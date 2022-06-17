a3de46679083b868cf8b2a04e86e1df123d4f9e8
Natália Dallabeneta <100920166+natydallabeneta@users.noreply.github.com>
2022-06-17 12:30:38 +0200

    Exercícios 18 e 19.

--- secao_02_estrutura_de_decisao/ex_18_validador_de_data.py
+++ secao_02_estrutura_de_decisao/ex_18_validador_de_data.py
@@ -24,3 +24,36 @@ Faça um Programa que peça uma data no formato dd/mm/aaaa e determine se a mesm
 
 def validar_data(data: str):
     """Escreva aqui em baixo a sua solução"""
+    try:
+        dia, mes, ano = map(int, data.split('/'))
+    except:
+        print("'Data inválida'")
+    else:
+        valida = False
+
+        dia, mes, ano = map(int, data.split('/'))
+        valida = False
+
+
+        # Meses com 31 dias
+        if mes == 1 or mes == 3 or mes == 5 or mes == 7 or mes == 8 or mes == 10 or mes == 12:
+            if 1 >= dia <= 31:
+                valida = True
+
+        # Meses com 30 dias
+        elif mes == 4 or mes == 6 or mes == 9 or mes == 11:
+            if dia <= 30:
+                valida = True
+
+        # se o mês for fevereiro, precisamos saber se ele é Bissexto
+        elif mes == 2:
+            if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
+                if dia <= 29:
+                    valida = True
+            elif dia <= 28:
+                valida = True
+
+        if valida:
+            print("'Data válida'")
+        else:
+            print("'Data inválida'")
--- secao_02_estrutura_de_decisao/ex_19_decomposicao_de_numero.py
+++ secao_02_estrutura_de_decisao/ex_19_decomposicao_de_numero.py
@@ -52,3 +52,56 @@ Observando os termos no plural a colocação do "e", da vírgula entre outros. E
 
 def decompor_numero(numero: int):
     """Escreva aqui em baixo a sua solução"""
+
+    centenas_str = dezenas_str = unidades_str = ''
+    numero_digitado = numero
+
+    if numero < 0:
+        print("'O número precisa ser positivo'")
+    elif numero > 999:
+        print("'O número precisa ser menor que 1000'")
+    else:
+        centenas_int, numero = divmod(numero, 100)
+
+    # calculo das partes numericas
+
+        partes_numericas = 0
+
+        if centenas_int == 1:
+            centenas_str = '1 centena'
+            partes_numericas += 1
+        elif centenas_int > 1:
+            centenas_str = f'{centenas_int} centenas'
+            partes_numericas += 1
+
+        dezenas_int, numero = divmod(numero, 10)
+
+        if dezenas_int == 1:
+            dezenas_str = '1 dezena'
+            partes_numericas += 1
+        elif dezenas_int > 1:
+            dezenas_str = f'{dezenas_int} dezenas'
+            partes_numericas += 1
+
+        if numero == 1:
+            unidades_str = '1 unidade'
+            partes_numericas += 1
+        elif numero > 1:
+            unidades_str = f'{numero} unidades'
+            partes_numericas += 1
+
+    # impressão de valores
+
+        if partes_numericas == 0:
+            print('O número 0 não possui centenas, dezenas ou unidades')
+        elif partes_numericas == 1:
+            print(f"'{numero_digitado} = {centenas_str}{dezenas_str}{unidades_str}'")
+        elif partes_numericas == 3:
+            print(f"'{numero_digitado} = {centenas_str}, {dezenas_str} e {unidades_str}'")
+        elif partes_numericas == 2:
+            if centenas_str != '':
+                segunda_parte = dezenas_str + unidades_str
+                print(f"'{numero_digitado} = {centenas_str} e {segunda_parte}'")
+
+            else:
+                print(f"'{numero_digitado} = { dezenas_str} e {unidades_str}'")

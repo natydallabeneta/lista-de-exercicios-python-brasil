516c41344ccd0e49ea67c6c4e973918704d8748d
Natália Dallabeneta <100920166+natydallabeneta@users.noreply.github.com>
2022-07-20 09:55:22 +0200

    Exercício 24

--- secao_03_estrutura_de_repeticao/ex_24_media_artmetica.py
+++ secao_03_estrutura_de_repeticao/ex_24_media_artmetica.py
@@ -7,9 +7,9 @@ Faça um programa que calcule o mostre a média aritmética de N notas.
     >>> calcular_media()
     'É necessária ao menos uma nota para calcular a média'
     >>> calcular_media(1)
-    1
+    1.0
     >>> calcular_media(1, 3)
-    2
+    2.0
     >>> calcular_media(1, 3, 3)
     2.3333333333333335
 
@@ -18,3 +18,11 @@ Faça um programa que calcule o mostre a média aritmética de N notas.
 
 def calcular_media(*notas) -> float:
     """Escreva aqui em baixo a sua solução"""
+
+    total = 0
+    if not notas:
+        return 'É necessária ao menos uma nota para calcular a média'
+    for i in notas:
+        total += i
+    media = total/len(notas)
+    return media

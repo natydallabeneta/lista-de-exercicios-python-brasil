d7005490a82c6b96d71a2b3974f19d935ce74c2d
Natália Dallabeneta <100920166+natydallabeneta@users.noreply.github.com>
2022-07-25 12:25:55 +0200

    Exercício 42

--- secao_03_estrutura_de_repeticao/ex_42_intervalos.py
+++ secao_03_estrutura_de_repeticao/ex_42_intervalos.py
@@ -6,27 +6,27 @@ Faça um programa que leia uma quantidade indeterminada de números positivos e 
 intervalos: [0-25], [26-50], [51-75] e [76-100].
 A entrada de dados deverá terminar quando for lido um número negativo.
 
-    >>> from secao_03_estrutura_de_repeticao import  ex_42_intervalos
+    >>> import  ex_42_intervalos
     >>> numeros_para_avaliacao=[-1, 10, 15, 20, 50, 13, 78, 22, 14, 16]
     >>> ex_42_intervalos.input = lambda k: numeros_para_avaliacao.pop()
     >>> ex_42_intervalos.listar_numeros_para_avaliacao()
     7 número(s) entre o intervalo de zero a 25
     1 número(s) entre o intervalo de 26 a 50
     1 número(s) entre o intervalo de 76 a 100
-    >>> from secao_03_estrutura_de_repeticao import  ex_42_intervalos
+    >>> import  ex_42_intervalos
     >>> numeros_para_avaliacao=[14, -5, 10, 2, 80, 99]
     >>> ex_42_intervalos.input = lambda k: numeros_para_avaliacao.pop()
     >>> ex_42_intervalos.listar_numeros_para_avaliacao()
     2 número(s) entre o intervalo de zero a 25
     2 número(s) entre o intervalo de 76 a 100
-    >>> from secao_03_estrutura_de_repeticao import  ex_42_intervalos
+    >>> import  ex_42_intervalos
     >>> numeros_para_avaliacao=[-55, 144, 5, 32, 18, 43, 12, 26, 76]
     >>> ex_42_intervalos.input = lambda k: numeros_para_avaliacao.pop()
     >>> ex_42_intervalos.listar_numeros_para_avaliacao()
     3 número(s) entre o intervalo de zero a 25
     3 número(s) entre o intervalo de 26 a 50
     1 número(s) entre o intervalo de 76 a 100
-    >>> from secao_03_estrutura_de_repeticao import  ex_42_intervalos
+    >>> import  ex_42_intervalos
     >>> numeros_para_avaliacao=[3, 5, 100, -5, 70, 88, 28, 12]
     >>> ex_42_intervalos.input = lambda k: numeros_para_avaliacao.pop()
     >>> ex_42_intervalos.listar_numeros_para_avaliacao()
@@ -40,3 +40,30 @@ A entrada de dados deverá terminar quando for lido um número negativo.
 
 def listar_numeros_para_avaliacao():
     """Escreva aqui em baixo a sua solução"""
+
+    numeros_para_avaliacao = 0
+    intervalo_de_zero_a_25 = 0
+    intervalo_de_26_a_50 = 0
+    intervalo_de_51_a_75 = 0
+    intervalo_de_76_a_100 = 0
+
+    while numeros_para_avaliacao >= 0:
+        numeros_para_avaliacao = int(input('Digite um número: '))
+        if 0 <= numeros_para_avaliacao <= 25:
+            intervalo_de_zero_a_25 += 1
+        elif 26 <= numeros_para_avaliacao <= 50:
+            intervalo_de_26_a_50 += 1
+        elif 51 <= numeros_para_avaliacao <= 75:
+            intervalo_de_51_a_75 += 1
+        elif 76 <= numeros_para_avaliacao <= 100:
+            intervalo_de_76_a_100 += 1
+
+    # print
+    if intervalo_de_zero_a_25 >= 1:
+        print(f'{intervalo_de_zero_a_25} número(s) entre o intervalo de zero a 25')
+    if intervalo_de_26_a_50 >= 1:
+        print(f'{intervalo_de_26_a_50} número(s) entre o intervalo de 26 a 50')
+    if intervalo_de_51_a_75 >= 1:
+        print(f'{intervalo_de_51_a_75} número(s) entre o intervalo de 51 a 75')
+    if intervalo_de_76_a_100 >= 1:
+        print(f'{intervalo_de_76_a_100} número(s) entre o intervalo de 76 a 100')

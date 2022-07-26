8f748a19750d91dcf5cbd6d29e89e002b18d6729
Natália Dallabeneta <100920166+natydallabeneta@users.noreply.github.com>
2022-07-26 16:23:22 +0200

    Exercícios 10 e 11

--- secao_04_exercicios_lista/ex_11_intercalacao_de_3_vetores.py
+++ secao_04_exercicios_lista/ex_11_intercalacao_de_3_vetores.py
@@ -19,3 +19,36 @@ Altere o programa anterior, intercalando 3 vetores.
 def intercalar(lista_1: list, lista_2: list, lista_3: list) -> list:
     """Escreva aqui em baixo a sua solução"""
 
+    if not lista_1 and not lista_2 and not lista_3:
+        return []
+
+    if len(lista_1) and len(lista_2) and len(lista_3) == 1:
+        return lista_1 + lista_2 + lista_3
+    else:
+
+        primeira_lista = lista_1
+        segunda_lista = lista_2
+        terceira_lista = lista_3
+
+        lista_intercalada = primeira_lista + segunda_lista + terceira_lista
+        lista_intercalada[::3] = primeira_lista
+        lista_intercalada[1::3] = segunda_lista
+        lista_intercalada[2::3] = terceira_lista
+
+        return lista_intercalada
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

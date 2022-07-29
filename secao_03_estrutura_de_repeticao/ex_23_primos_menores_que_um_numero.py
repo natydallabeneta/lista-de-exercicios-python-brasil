dfa8f707cdee012759b18b012c21190d9fd77d46
Natália Dallabeneta <100920166+natydallabeneta@users.noreply.github.com>
2022-07-29 11:33:15 +0200

    Exercício 23

--- secao_03_estrutura_de_repeticao/ex_23_primos_menores_que_um_numero.py
+++ secao_03_estrutura_de_repeticao/ex_23_primos_menores_que_um_numero.py
@@ -1,11 +1,9 @@
 """
 Exercício 23 da seção de estrutura sequencial da Python Brasil:
 https://wiki.python.org.br/EstruturaDeRepeticao
-
 Faça um programa que mostre todos os primos entre 1 e N sendo N um número inteiro fornecido pelo usuário.
 O programa deverá mostrar também o número de divisões que ele executou para encontrar os números primos.
 Serão avaliados o funcionamento, o estilo e o número de testes (divisões) executados.
-
     >>> primos, divisoes = calcular_primos_e_divisoes(0)
     >>> primos
     ''
@@ -36,7 +34,6 @@ Serão avaliados o funcionamento, o estilo e o número de testes (divisões) exe
     '2, 3, 5'
     >>> divisoes <= 6
     True
-
 """
 from typing import Tuple
 
@@ -44,19 +41,28 @@ from typing import Tuple
 def calcular_primos_e_divisoes(n: int) -> Tuple[str, int]:
     """Escreva aqui em baixo a sua solução"""
 
-    numero = int(input("\nDigite um número: "))
-    lista = []
-    divisoes = 0
+    retorno_2 = 0
+    retorno_1 = []
+    # Passa por todos os números até o limite superior definido pelo usuario
+    # Para cada número, vai testar se ele é primo
+    # No teste não é considerado como 'divisoes' quando um número é divido por 1 e por dois, por isso o contador do
+    # código inicia em -2.
 
-    for i in range(numero + 1):
-        if i % 2 == 1 and i != 2:
-            lista.append(i)
-            divisoes += 1
+    for numero in range(2, n + 1):
+
+        contador = 0
+        divisoes = -2
+
+        if numero >= 2:
+            for divisor in range(1, numero + 1):
+                divisoes += 1
+                if numero % divisor == 0:
+                    contador += 1
+        if contador == 2:
+            retorno_1.append(str(numero))
+            retorno_2 += divisoes
         else:
-            divisoes += 1
-    print("Números primos: ", lista)
-    print("Número de divisões", divisoes)
+            retorno_2 += divisoes
 
-
-
-
+    retorno = [', '.join(retorno_1), retorno_2]
+    return retorno
